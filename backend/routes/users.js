1d3e49d8e1e663f39d3c45c5cd43039ef4dfec04
MaxJeong <max.jeong@mail.utoronto.ca>
2020-03-09 02:34:15 -0400

    finished the backend

---
+++ backend/routes/users.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+//first endpoint that handles incoming HTTP GET requests on /users/
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//second endpoint that handles incoming HTTP POST requests on /users/add
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//database uri
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+
+//outputs message once the connection is opened
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+//import the files into the variables
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+//when accessing root url with these, it will load the files respectively
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
