63cd4ad7c3f08f1d119db9bdef94027c0f523e43
MaxJeong <max.jeong@mail.utoronto.ca>
2020-03-12 05:12:44 -0400

    finished designing components for the app. also connected the frontend to the backend

---
+++ src/components/create-user.component.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateUsers extends Component {
+    constructor(props) {
+        //Must call super for all React component classes that have a constructor
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        //This is how you declare variables in React
+        this.state = {
+            username: '',
+        }
+    }
+
+    //target is the textbox and the value is the value in the textbox
+    //this method will set the value of username to the value in the textbox
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        //prevents the default html form submit behaviour
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        }
+
+        console.log(user);
+
+        //sends a post request to the backend endpoint
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
+
+        //set username to blank so user can type in a new username
+        this.setState({
+            username: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class EditExercises extends Component {
+    constructor(props) {
+        //Must call super for all React component classes that have a constructor
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        //This is how you declare variables in React
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    //this functions calls right before anything display on the web page
+    componentDidMount() {
+        //gets the id directly from the url
+        axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    duration: response.data.duration,
+                    date: new Date(response.data.date)
+                })   
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        //this will fetch only the username field from the user object
+                        users: response.data.map(user => user.username),
+                    })
+                }
+            })
+    }
+
+    //target is the textbox and the value is the value in the textbox
+    //this method will set the value of username to the value in the textbox
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        //prevents the default html form submit behaviour
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        //sends a post request to the backend endpoint
+        axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
+            .then(res => console.log(res.data));
+
+        //takes the user back to the home page
+        window.location = '/';
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Edit Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <label>Username: </label>
+                        <select ref="userInput"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}>
+                            {
+                                this.state.users.map(function(user) {
+                                    return <option 
+                                        key={user}
+                                        value={user}>{user}
+                                        </option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group"> 
+                        <label>Description: </label>
+                        <input  type="text"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration (in minutes): </label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            value={this.state.duration}
+                            onChange={this.onChangeDuration}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                        <DatePicker
+                            selected={this.state.date}
+                            onChange={this.onChangeDate}
+                        />
+                        </div>
+                    </div>
+            
+                    <div className="form-group">
+                        <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
